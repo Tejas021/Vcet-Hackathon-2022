98356d73a48c3ec9ef51bd9e0cab5e30a52d612c
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2022-08-14 23:09:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Landing from './components/Landing'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Welcome to VCET Hackathon 2022</h1>
+      <Landing/>
+    </div>
+  )
+}
+
+export default App
