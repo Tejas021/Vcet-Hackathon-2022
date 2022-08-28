23ace839cb34cf04f30cc8b4aa29af7f92ad4cfc
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2022-08-28 21:01:32 +0530

    router and form

---
+++ src/pages/Form/Form.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import styles from "./Form.module.css"
+const Form = () => {
+
+const submitInfo=(e)=>{
+e.preventDefault();
+console.log(data);
+
+}
+
+  return (
+    <div className='p-5'>
+    <Navbar/>
+    <h1 className={`text-center ${styles.textPurple}`}>Register</h1>
+      <form className={`form-control p-5 ${styles.bgBlue} ${styles.form1}`}>
+
+      <div className='p-2'>
+      <h3 className={`text-light`}>Team Leader</h3>
+      <input className={`form-control mt-2`} placeholder="Name"/>
+      <input className={`form-control mt-2`} placeholder="Surname"/>
+
+      </div>
+    
+
+      <button className={`btn ${styles.bgPurple} text-light`} type="submit" onClick={(e)=>submitInfo(e)}>Submit</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Gallery from '../../components/Gallery/Gallery'
+import Landing from '../../components/Landing/Landing'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Landing/>
+      <Gallery/>
+    </div>
+  )
+}
+
+export default Home
