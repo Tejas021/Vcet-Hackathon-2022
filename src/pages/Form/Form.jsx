a6abe52941ac4f503550128c2cf0819a10bb380e
Tanmay-Patel-21 <tanmay.202174101@vcet.edu.in>
2022-08-30 11:34:28 +0530

    Updated Sponsors component

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -3,27 +3,27 @@ import Navbar from '../../components/Navbar/Navbar'
 import styles from "./Form.module.css"
 const Form = () => {
 
-const submitInfo=(e)=>{
-e.preventDefault();
-console.log(data);
+  const submitInfo = (e) => {
+    e.preventDefault();
+    console.log(data);
 
-}
+  }
 
   return (
     <div className='p-5'>
-    <Navbar/>
-    <h1 className={`text-center ${styles.textPurple}`}>Register</h1>
+      <Navbar />
+      <h1 className={`text-center ${styles.textPurple}`}>Register</h1>
       <form className={`form-control p-5 ${styles.bgBlue} ${styles.form1}`}>
 
-      <div className='p-2'>
-      <h3 className={`text-light`}>Team Leader</h3>
-      <input className={`form-control mt-2`} placeholder="Name"/>
-      <input className={`form-control mt-2`} placeholder="Surname"/>
+        <div className='p-2'>
+          <h3 className={`text-light`}>Team Leader</h3>
+          <input className={`form-control mt-2`} placeholder="Name" />
+          <input className={`form-control mt-2`} placeholder="Surname" />
 
-      </div>
-    
+        </div>
 
-      <button className={`btn ${styles.bgPurple} text-light`} type="submit" onClick={(e)=>submitInfo(e)}>Submit</button>
+
+        <button className={`btn ${styles.bgPurple} text-light`} type="submit" onClick={(e) => submitInfo(e)}>Submit</button>
 
       </form>
     </div>
