430f1a0a960851bdb230adca1efbcf9099ba247f
Shiv <pals55002@gmail.com>
2022-09-01 20:59:45 +0530

    Faq done

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./Faq.css"
 function Faq() {
   return (
-    <div className="mb-5">
+    <div className="mb-5 text-light">
       <i className="fas fa-bell-slash"></i>
       <div className="container mt-5 ">
         <h1 className="text-center">FAQ</h1>
         <div className="accordion mt-5" id="accordionFlushExample">
           <div className="row ">
             <div className="col-lg-6 col-md-6 col-xs-12 col-sm-12  ">
-              <div className="accordion-item border bg-light">
+              <div className=" accordion-item border border-dark bg-light">
                 <h2 className="accordion-header" id="flush-headingOne">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseOne"
@@ -27,16 +28,16 @@ function Faq() {
                   aria-labelledby="flush-headingOne"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     VCET Hackathon’21 Reloaded will be held online.
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border  border-dark">
                 <h2 className="accordion-header" id="flush-headingTwo">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseTwo"
@@ -52,7 +53,7 @@ function Faq() {
                   aria-labelledby="flush-headingTwo"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Anyone is welcome to participate in this event. While
                     experienced coding and programming is a huge plus, teams
                     will also need people with strong presentation skills and
@@ -61,10 +62,10 @@ function Faq() {
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingThree">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseThree"
@@ -80,17 +81,17 @@ function Faq() {
                   aria-labelledby="flush-headingThree"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     No, every member of the team must be a current student of a
                     university or a college. (Members from different colleges
                     allowed).
                   </div>
                 </div>
               </div>
-              <div className="accordion-item border">
-                <h2 className="accordion-header" id="flush-headingfour">
+              <div className="accordion-item border border-dark">
+                <h2 className="accordion-header " id="flush-headingfour">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapsefour"
@@ -106,17 +107,17 @@ function Faq() {
                   aria-labelledby="flush-headingfour"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Problem Statements will be provided from our end. You have
                     to submit abstracts for all 5 Problem Statements, out of
                     which 1 will be allocated.
                   </div>
                 </div>
               </div>
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingfive">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapsefive"
@@ -133,16 +134,16 @@ function Faq() {
                   aria-labelledby="flush-headingfive"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Entry fee is 500/- per team and the last date of
                     registration is September 19, 2021.
                   </div>
                 </div>
               </div>
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingSix">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseSix"
@@ -158,7 +159,7 @@ function Faq() {
                   aria-labelledby="flush-headingSix"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Which payment methods do you accept?
                   </div>
                 </div>
@@ -166,10 +167,10 @@ function Faq() {
             </div>
 
             <div className="col-lg-6 col-md-6 col-xs-12 col-sm-12 ">
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingSeven">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseSeven"
@@ -185,16 +186,16 @@ function Faq() {
                   aria-labelledby="flush-headingSeven"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     No, the data has to be collected on your own.
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingEight">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseEight"
@@ -210,17 +211,17 @@ function Faq() {
                   aria-labelledby="flush-headingEight"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Innovation, Technology, Completion & Business Values are
                     some of the criterias.
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingNine">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseNine"
@@ -236,16 +237,16 @@ function Faq() {
                   aria-labelledby="flush-headingNine"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Google Meet, YouTube live streaming and Discord.
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingTen">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseTen"
@@ -261,17 +262,17 @@ function Faq() {
                   aria-labelledby="flush-headingTen"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     Results will be announced at the end of the Final Pitching
                     round. (Google Meet & YouTube live stream)
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingEleven">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseEleven"
@@ -287,17 +288,17 @@ function Faq() {
                   aria-labelledby="flush-headingEleven"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     There is no cancellation policy and payment once done will
                     not be refunded.
                   </div>
                 </div>
               </div>
 
-              <div className="accordion-item border">
+              <div className="accordion-item border border-dark">
                 <h2 className="accordion-header" id="flush-headingTwelve">
                   <button
-                    className="accordion-button collapsed"
+                    className="accordion-button button-light collapsed text-light"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#flush-collapseTwelve"
@@ -313,7 +314,7 @@ function Faq() {
                   aria-labelledby="flush-headingTwelve"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
+                  <div className="accordion-body text-light">
                     You can contact us via mail or drop your query in the
                     comment sections on our Social Media handles.
                   </div>
