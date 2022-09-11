5262f1737d97eeae79c4fb4616664db1797a2232
Tanmay-Patel-21 <tanmay.202174101@vcet.edu.in>
2022-09-11 19:01:21 +0530

    Registration form added

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import styles from "./Form.module.css"
+import './Form.css'
+
 const Form = () => {
 
   const submitInfo = (e) => {
@@ -8,25 +9,132 @@ const Form = () => {
     console.log(data);
 
   }
-
   return (
-    <div className='p-5'>
+    <>
       <Navbar />
-      <h1 className={`text-center ${styles.textPurple}`}>Register</h1>
-      <form className={`form-control p-5 ${styles.bgBlue} ${styles.form1}`}>
+      <div className="form-main-container form-grid">
+        <div className="team-leader-container">
+          <div className="details-container form-grid">
+            <label className="form-heading" for="id_team_name">
+              Team Name
+              <span style={{ "color": "tomato" }}>*</span>
+            </label>
+            <input type="text" className="form-control" id="id_team_name" name="team_name" placeholder="Team Name"
+              data-error="Please enter your name" value required />
+            <label className="form-heading" for="id_team_name">
+              Team Leader
+              <span style={{ "color": "tomato" }}>*</span>
+            </label>
+            <input type="text" className="form-control" id="id_team_leader_name" name="team_leader_name"
+              placeholder="Full Name" data-error="Please enter your team name" value required />
 
-        <div className='p-2'>
-          <h3 className={`text-light`}>Team Leader</h3>
-          <input className={`form-control mt-2`} placeholder="Name" />
-          <input className={`form-control mt-2`} placeholder="Surname" />
+            <input type="text" className="form-control" id="id_team_leader_college_name" name="team_leader_college_name"
+              placeholder="College Name" data-error="Please enter your team name" value required />
 
+            <input type="email" className="form-control email-input" id="id_team_leader_email" name="team_leader_email"
+              placeholder="Email" data-error="Please enter your team name" value required />
+
+            <input type="text" className="form-control half-form course-year" id="id_team_leader_course_year"
+              name="team_leader_course_year" placeholder="Course and Year"
+              data-error="Please enter your team name" value required />
+            <input type="text" className="form-control half-form tel-input" id="id_team_leader_tel_number"
+              name="team_leader_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
+              value required />
+
+
+            <input type="text" className="form-control half-form alt-tel-input" id="id_team_leader_alter_tel_number"
+              name="team_leader_alter_tel_number" placeholder="Alternate Phone No."
+              data-error="Please enter your team name" value required />
+            <select className="form-control half-form" name="team_leader_gender" id="id_team_leader_gender">
+              <option className="form-control" selected disabled>Gender</option>
+              <option className="form-control" value="M">Male</option>
+              <option className="form-control" value="F">Female</option>
+            </select>
+          </div>
         </div>
-
-
-        <button className={`btn ${styles.bgPurple} text-light`} type="submit" onClick={(e) => submitInfo(e)}>Submit</button>
-
-      </form>
-    </div>
+        <div className="team-member1-container">
+          <div className="details-container form-grid">
+            <label className="form-heading" for="id_teammate1_name">
+              Teammate 1
+            </label>
+            <input type="text" className="form-control" id="id_teammate1_name" name="teammate1_name"
+              placeholder="Full Name" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control" id="id_teammate1_college_name" name="teammate1_college_name"
+              placeholder="College Name" data-error="Please enter your team name" value required />
+            <input type="email" className="form-control email-input" id="id_teammate1_email" name="teammate1_email"
+              placeholder="Email" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control half-form course-year" id="id_teammate1_course_year"
+              name="teammate1_course_year" placeholder="Course and Year" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form tel-input" id="id_teammate1_tel_number"
+              name="teammate1_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form alt-tel-input" id="id_teammate1_alter_tel_number"
+              name="teammate1_alter_tel_number" placeholder="Alternate Phone No."
+              data-error="Please enter your team name" value required />
+            <select className="form-control half-form" name="teammate1_gender" id="id_teammate1_gender">
+              <option selected disabled>Gender</option>
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select>
+          </div>
+        </div>
+        <div className="team-member2-container">
+          <div className="details-container form-grid">
+            <label className="form-heading" for="id_teammate2_name">
+              Teammate 2
+            </label>
+            <input type="text" className="form-control" id="id_teammate2_name" name="teammate2_name"
+              placeholder="Full Name" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control" id="id_teammate2_college_name" name="teammate2_college_name"
+              placeholder="College Name" data-error="Please enter your team name" value required />
+            <input type="email" className="form-control email-input" id="id_teammate2_email" name="teammate2_email"
+              placeholder="Email" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control half-form course-year" id="id_teammate2_course_year"
+              name="teammate2_course_year" placeholder="Course and Year" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form tel-input" id="id_teammate2_tel_number"
+              name="teammate2_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form alt-tel-input" id="id_teammate2_alter_tel_number"
+              name="teammate2_alter_tel_number" placeholder="Alternate Phone No."
+              data-error="Please enter your team name" value required />
+            <select className="form-control half-form" name="teammate2_gender" id="id_teammate2_gender">
+              <option selected disabled>Gender</option>
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select>
+          </div>
+        </div>
+        <div className="team-member3-container">
+          <div className="details-container form-grid">
+            <label className="form-heading" for="id_teammate3_name">
+              Teammate 3
+            </label>
+            <input type="text" className="form-control" id="id_teammate3_name" name="teammate3_name"
+              placeholder="Full Name" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control" id="id_teammate3_college_name" name="teammate3_college_name"
+              placeholder="College Name" data-error="Please enter your team name" value required />
+            <input type="email" className="form-control email-input" id="id_teammate3_email" name="teammate3_email"
+              placeholder="Email" data-error="Please enter your team name" value required />
+            <input type="text" className="form-control half-form course-year" id="id_teammate3_course_year"
+              name="teammate3_course_year" placeholder="Course and Year" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form tel-input" id="id_teammate3_tel_number"
+              name="teammate3_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
+              value required />
+            <input type="text" className="form-control half-form alt-tel-input" id="id_teammate3_alter_tel_number"
+              name="teammate3_alter_tel_number" placeholder="Alternate Phone No."
+              data-error="Please enter your team name" value required />
+            <select className="form-control half-form" name="teammate3_gender" id="id_teammate3_gender">
+              <option className="form-control" selected disabled>Gender</option>
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
