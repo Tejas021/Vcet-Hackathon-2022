1bf93df460b84ecbb3035f553cc6cf7766549583
unknown <akshayhegde158@gmail.com>
2022-09-13 13:39:25 +0530

    prizes

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -4,11 +4,11 @@ import './Form.css'
 
 const Form = () => {
 
-  const submitInfo = (e) => {
-    e.preventDefault();
-    console.log(data);
+  // const submitInfo = (e) => {
+  //   e.preventDefault();
+  //   console.log(data);
 
-  }
+  // }
   return (
     <>
       <Navbar />
@@ -28,7 +28,7 @@ const Form = () => {
                     <option className="form-control" value="">Recommendation for career/resume building</option>
                 </select>
                 <textarea className="form-control" name="" id="" cols="" rows=""></textarea>
-                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" value required/>
+                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" required/>
                 <select className="form-control " name="" id="">
                     <option className="form-control" selected disabled>Preference</option>
                     <option className="form-control" value="">Preference 1</option>
@@ -49,7 +49,7 @@ const Form = () => {
                     <option className="form-control" value=""> CRM for Business </option>
                 </select>
                 <textarea className="form-control" name="" id="" cols="" rows=""></textarea>
-                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" value required />
+                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" required />
                 <select className="form-control " name="" id="">
                     <option className="form-control" selected disabled>Preference</option>
                     <option className="form-control" value="">Preference 1</option>
@@ -70,7 +70,7 @@ const Form = () => {
                     <option className="form-control" value="">Email monitoring</option>
                 </select>
                 <textarea className="form-control" name="" id="" cols="" rows=""></textarea>
-                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" value required />
+                <input type="text" name="" id="" className="form-control" placeholder="Technology Stack" required />
                 <select className="form-control " name="" id="">
                     <option className="form-control" selected disabled>Preference</option>
                     <option className="form-control" value="">Preference 1</option>
@@ -91,31 +91,31 @@ const Form = () => {
                 <span style={{ "color": "tomato" }}>*</span>
               </label>
               <input type="text" className="form-control" id="id_team_name" name="team_name" placeholder="Team Name"
-                data-error="Please enter your name" value required />
+                data-error="Please enter your name" required />
               <label className="form-heading" for="id_team_name">
                 Team Leader
                 <span style={{ "color": "tomato" }}>*</span>
               </label>
               <input type="text" className="form-control" id="id_team_leader_name" name="team_leader_name"
-                placeholder="Full Name" data-error="Please enter your team name" value required />
+                placeholder="Full Name" data-error="Please enter your team name"  required />
 
               <input type="text" className="form-control" id="id_team_leader_college_name" name="team_leader_college_name"
-                placeholder="College Name" data-error="Please enter your team name" value required />
+                placeholder="College Name" data-error="Please enter your team name" required />
 
               <input type="email" className="form-control email-input" id="id_team_leader_email" name="team_leader_email"
-                placeholder="Email" data-error="Please enter your team name" value required />
+                placeholder="Email" data-error="Please enter your team name" required />
 
               <input type="text" className="form-control half-form course-year" id="id_team_leader_course_year"
                 name="team_leader_course_year" placeholder="Course and Year"
-                data-error="Please enter your team name" value required />
+                data-error="Please enter your team name" required />
               <input type="text" className="form-control half-form tel-input" id="id_team_leader_tel_number"
                 name="team_leader_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
-                value required />
+                required />
 
 
               <input type="text" className="form-control half-form alt-tel-input" id="id_team_leader_alter_tel_number"
                 name="team_leader_alter_tel_number" placeholder="Alternate Phone No."
-                data-error="Please enter your team name" value required />
+                data-error="Please enter your team name" required />
               <select className="form-control half-form" name="team_leader_gender" id="id_team_leader_gender">
                 <option className="form-control" selected disabled>Gender</option>
                 <option className="form-control" value="M">Male</option>
@@ -129,20 +129,20 @@ const Form = () => {
                 Teammate 1
               </label>
               <input type="text" className="form-control" id="id_teammate1_name" name="teammate1_name"
-                placeholder="Full Name" data-error="Please enter your team name" value required />
+                placeholder="Full Name" data-error="Please enter your team name" required />
               <input type="text" className="form-control" id="id_teammate1_college_name" name="teammate1_college_name"
-                placeholder="College Name" data-error="Please enter your team name" value required />
+                placeholder="College Name" data-error="Please enter your team name" required />
               <input type="email" className="form-control email-input" id="id_teammate1_email" name="teammate1_email"
-                placeholder="Email" data-error="Please enter your team name" value required />
+                placeholder="Email" data-error="Please enter your team name" required />
               <input type="text" className="form-control half-form course-year" id="id_teammate1_course_year"
                 name="teammate1_course_year" placeholder="Course and Year" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form tel-input" id="id_teammate1_tel_number"
                 name="teammate1_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form alt-tel-input" id="id_teammate1_alter_tel_number"
                 name="teammate1_alter_tel_number" placeholder="Alternate Phone No."
-                data-error="Please enter your team name" value required />
+                data-error="Please enter your team name" required />
               <select className="form-control half-form" name="teammate1_gender" id="id_teammate1_gender">
                 <option selected disabled>Gender</option>
                 <option value="M">Male</option>
@@ -156,20 +156,20 @@ const Form = () => {
                 Teammate 2
               </label>
               <input type="text" className="form-control" id="id_teammate2_name" name="teammate2_name"
-                placeholder="Full Name" data-error="Please enter your team name" value required />
+                placeholder="Full Name" data-error="Please enter your team name" required />
               <input type="text" className="form-control" id="id_teammate2_college_name" name="teammate2_college_name"
-                placeholder="College Name" data-error="Please enter your team name" value required />
+                placeholder="College Name" data-error="Please enter your team name" required />
               <input type="email" className="form-control email-input" id="id_teammate2_email" name="teammate2_email"
-                placeholder="Email" data-error="Please enter your team name" value required />
+                placeholder="Email" data-error="Please enter your team name" required />
               <input type="text" className="form-control half-form course-year" id="id_teammate2_course_year"
                 name="teammate2_course_year" placeholder="Course and Year" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form tel-input" id="id_teammate2_tel_number"
                 name="teammate2_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form alt-tel-input" id="id_teammate2_alter_tel_number"
                 name="teammate2_alter_tel_number" placeholder="Alternate Phone No."
-                data-error="Please enter your team name" value required />
+                data-error="Please enter your team name" required />
               <select className="form-control half-form" name="teammate2_gender" id="id_teammate2_gender">
                 <option selected disabled>Gender</option>
                 <option value="M">Male</option>
@@ -183,20 +183,20 @@ const Form = () => {
                 Teammate 3
               </label>
               <input type="text" className="form-control" id="id_teammate3_name" name="teammate3_name"
-                placeholder="Full Name" data-error="Please enter your team name" value required />
+                placeholder="Full Name" data-error="Please enter your team name" required />
               <input type="text" className="form-control" id="id_teammate3_college_name" name="teammate3_college_name"
-                placeholder="College Name" data-error="Please enter your team name" value required />
+                placeholder="College Name" data-error="Please enter your team name" required />
               <input type="email" className="form-control email-input" id="id_teammate3_email" name="teammate3_email"
-                placeholder="Email" data-error="Please enter your team name" value required />
+                placeholder="Email" data-error="Please enter your team name" required />
               <input type="text" className="form-control half-form course-year" id="id_teammate3_course_year"
                 name="teammate3_course_year" placeholder="Course and Year" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form tel-input" id="id_teammate3_tel_number"
                 name="teammate3_tel_number" placeholder="Phone Number" data-error="Please enter your team name"
-                value required />
+                required />
               <input type="text" className="form-control half-form alt-tel-input" id="id_teammate3_alter_tel_number"
                 name="teammate3_alter_tel_number" placeholder="Alternate Phone No."
-                data-error="Please enter your team name" value required />
+                data-error="Please enter your team name" required />
               <select className="form-control half-form" name="teammate3_gender" id="id_teammate3_gender">
                 <option className="form-control" selected disabled>Gender</option>
                 <option value="M">Male</option>
