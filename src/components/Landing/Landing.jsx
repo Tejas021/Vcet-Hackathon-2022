30a3b6f8cb2da6bba581e4d65ece6a9a1856c098
Raj <rajsrivastav0603@gmail.com>
2022-09-13 12:18:14 +0530

    landing updates

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import styles from "./Landing.module.css";
-const Landing = () => {
-  return (
-    <div className={`d-flex flex-column justify-content-center ${styles.LandingContainer}`}>
+import "./Landing.scss"
 
+function Landing() {
+ const handleMouseMove = (e) => {
+    const el = document.getElementById("wrapper");
+    const d = el.getBoundingClientRect();
+    let x = e.clientX - (d.left + Math.floor(d.width / 2));
+    let y = e.clientY - (d.top + Math.floor(d.height / 2));
+    // Invert values
+    x = x - x * 2;
+    y = y - y * 2;
+    document.documentElement.style.setProperty("--scale", 1.6);
+    document.documentElement.style.setProperty("--x", x / 2 + "px");
 
-      <h1 >VidyaVardhini's College Of Engineering and Technolgy </h1>
-      <p className='text-center text-light'>presents</p>
-      <h1 className='fw-bold'>VCET HACKATHON</h1>
-      <h3 className='text-center text-light fw-bold'>Multiverse of Coding</h3>
-    </div>
-  )
-}
+    document.documentElement.style.setProperty("--y", y / 2 + "px");
+  }
+
+  const handleMouseLeave = () => {
+    document.documentElement.style.setProperty("--scale", 1);
+    document.documentElement.style.setProperty("--x", 0);
+    document.documentElement.style.setProperty("--y", 0);
+  };
+  
+    return (
+      <div className="main">
+
+      <div
+        id="wrapper"
+        onMouseMove={(e)=>{handleMouseMove(e)}}
+        onClick={(e)=>{handleMouseLeave(e)}}
+      >
+        <img id="image" />
+      </div>
+      </div>
+    );
+  }
 
 export default Landing
