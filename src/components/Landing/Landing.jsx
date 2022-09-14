f2ae11d0a8c5083b1d67eb568a915abdac956cc6
Tanmay-Patel-21 <tanmay.202174101@vcet.edu.in>
2022-09-14 22:03:57 +0530

    landing page updated with new banground

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <div className={`d-flex flex-column justify-content-center ${styles.LandingContainer}`} id="Landing">
       <div className={styles.landingContainer}>
         <h1 className={styles.collegeName}>Vidyavardhini's College Of Engineering and Technology </h1>
-        <p className={`text-center mt-4 ${styles.themeName}`} style={{color:"white"}}><h4>presents</h4></p>
+        <p className={`text-center mt-4 ${styles.themeName}`}><h4>presents</h4></p>
         <h1 className={styles.mainHeading}>VCET HACKATHON</h1>
         <h3 className={styles.themeName}>Multiverse of Hacking</h3>
       </div>
