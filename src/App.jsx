68407c8dc7dd99492094385de2d856e67048f2b2
unknown <akshayhegde158@gmail.com>
2022-09-19 18:28:35 +0530

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 
 import './App.css'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import './App.module.css'
 import Home from './pages/Home/Home'
 import Form from './pages/Form/Form'
-
+import { addDoc, collection, db, getDocs } from './firebase'
 
 function App() {
 
   const [success,setSuccess]=useState(false)
+  const [l,setL]=useState([])
+
+  useEffect(() => {
+    const lists = async () => {
+      const listRef = collection(db, "registrations");
+      const querySnapshot = await getDocs(listRef);
+      let arr = []
+      querySnapshot.docs.map(doc => {
+        arr = arr.concat(doc.data().data)
+      })
+      setL(arr)
+    }
+    lists()
+
+  }, [])
+
+console.log(l)
 
   return (
 
