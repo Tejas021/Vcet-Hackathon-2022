f9012f170ffb5bff60090bb58213462096c5a390
Raj <rajsrivastav0603@gmail.com>
2022-09-20 00:17:55 +0530

    prizepool updated

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-import "./Landing.scss"
-import "./Landing.css"
-
-function Landing() {
-  const handleMouseMove = (e) => {
-    const el = document.getElementById("wrapper");
-    const d = el.getBoundingClientRect();
-    let x = e.clientX - (d.left + Math.floor(d.width / 2));
-    let y = e.clientY - (d.top + Math.floor(d.height / 2));
-    // Invert values
-    x = x - x * 2;
-    y = y - y * 2;
-    document.documentElement.style.setProperty("--scale", 1.45);
-    document.documentElement.style.setProperty("--x", x / 2 + "px");
-
-    document.documentElement.style.setProperty("--y", y / 2 + "px");
-  }
-
-  const handleMouseLeave = () => {
-    document.documentElement.style.setProperty("--scale", 1);
-    document.documentElement.style.setProperty("--x", 0);
-    document.documentElement.style.setProperty("--y", 0);
-  };
-
-  return (
-    <div className="main">
-
-      <div
-        id="wrapper"
-        onMouseMove={(e) => { handleMouseMove(e) }}
-        onClick={(e) => { handleMouseLeave(e) }}
-      >
-        <img id="image" />
-        <div className="landingContainer" >
-            <h1 className="collegeName">Vidyavardhini's College Of Engineering and Technology </h1>
-            <p className={`text-center mt-4 presents`}>presents</p>
-            <h1 className="mainHeading">VCET HACKATHON</h1>
-            <h3 className="themeName">Multiverse of Hacking</h3>
-          </div>
-        {/* <div className="d-flex flex-column justify-content-center LandingContainer " id="Landing" >
-          
-        </div> */}
-      </div>
-      <div className="mobile">
-
-=======
 import React from 'react'
 import styles from "./Landing.module.css";
 import { Link } from "react-router-dom";
@@ -61,13 +14,12 @@ const Landing = () => {
         <h1 className={styles.mainHeading}>VCET HACKATHON</h1>
         <h3 className={styles.themeName}>Multiverse of Hacking</h3>
        
->>>>>>> 1784c1ee65bd752755272af63e17a5a50514342a
       </div>
       <div className={styles.RegisterBtnDiv}>
         <Link to="/register" className={`${styles.linktoreg}`}><button className={`  ${styles.RegisterBtn} btn text-light mx-4 my-2 p-3 px-4`}>REGISTER NOW</button></Link>
         </div>
     </div>
-  );
+  )
 }
 
 export default Landing
---
+++ src/components/PP/PP.jsx
@@ -0,0 +1,90 @@
+import Prize from "../../assets/prizes/prize.png"
+import winner from "../../assets/prizes/winner.png"
+import second from "../../assets/prizes/secondimg.png"
+import bronze from "../../assets/prizes/bronze.png"
+import "./PP.css"
+
+function PP() {
+    return (
+        <>
+            <div className="pp-container">
+                <div className="pp-c1">
+
+                    <img src={Prize} alt="" className="pp-img" />
+                </div>
+                <div className="pp-c2">
+                    <p className="pp-heading">MULTIVERSE OF HACKING</p>
+                    <p className="pp-pool">PRIZE POOL</p>
+                    <p className="pp-amount">&#x20b9; 60,000</p>
+                    <p className="pp-date">HACK IT ON 6TH & 8TH OCT</p>
+
+                </div>
+
+            </div>
+
+            <div className="pm">
+                <div className="first-pm">
+                    <img src={winner} alt="" className="first-pm-img" />
+                    <div className="first-pm-text">
+                        <span className="first-pm-prize"> 1<span style={{ fontSize: "x-large" }}>st</span> prize </span> <br />
+                        <span className="first-pm-amount">25,000</span>
+
+                    </div>
+                </div>
+
+                <div className="other-pm">
+                    <div className="second-pm">
+                        <img src={second} alt="" className="other-pm-img" />
+                        <div className="second-pm-text">
+                            <span className="second-pm-prize"> 2<span style={{ fontSize: "large" }}>nd</span> prize </span> <br />
+                            <span className="second-pm-amount">15,000</span>
+
+                        </div>
+                    </div>
+                    <div className="remaining-pm">
+                        <div className="third-pm-top">
+
+                            <div className="third-pm">
+                                <img src={bronze} alt="" className="third-pm-img" />
+                                <div className="third-pm-text">
+                                    <span className="third-pm-prize"> 3<span style={{ fontSize: "medium" }}>rd</span> prize </span> <br />
+                                    <span className="third-pm-amount">5,000</span>
+
+                                </div>
+                            </div>
+                            <div className="third-pm" style={{marginLeft: "150px"}}>
+                                <img src={bronze} alt="" className="third-pm-img" />
+                                <div className="third-pm-text">
+                                    <span className="third-pm-prize"> 4<span style={{ fontSize: "medium" }}>th</span> prize </span> <br />
+                                    <span className="third-pm-amount">5,000</span>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="third-pm-bottom">
+
+                            <div className="third-pm">
+                                <img src={bronze} alt="" className="third-pm-img" />
+                                <div className="third-pm-text">
+                                    <span className="third-pm-prize"> 5<span style={{ fontSize: "medium" }}>th</span> prize </span> <br />
+                                    <span className="third-pm-amount">5,000</span>
+
+                                </div>
+                            </div>
+                            <div className="third-pm">
+                                <img src={bronze} alt="" className="third-pm-img" />
+                                <div className="third-pm-text">
+                                    <span className="third-pm-prize"> 6<span style={{ fontSize: "medium" }}>th</span> prize </span> <br />
+                                    <span className="third-pm-amount">5,000</span>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default PP
