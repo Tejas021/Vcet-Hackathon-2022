817cb8611a2a8dbfd81d8aaedaa0817f1cd09717
Raj <rajsrivastav0603@gmail.com>
2022-09-19 11:51:50 +0530

    Landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import "./Landing.scss"
 import "./Landing.css"
 
 function Landing() {
- const handleMouseMove = (e) => {
+  const handleMouseMove = (e) => {
     const el = document.getElementById("wrapper");
     const d = el.getBoundingClientRect();
     let x = e.clientX - (d.left + Math.floor(d.width / 2));
@@ -21,26 +21,31 @@ function Landing() {
     document.documentElement.style.setProperty("--x", 0);
     document.documentElement.style.setProperty("--y", 0);
   };
-  
-    return (
-      <div className="main">
+
+  return (
+    <div className="main">
 
       <div
         id="wrapper"
-        onMouseMove={(e)=>{handleMouseMove(e)}}
-        onClick={(e)=>{handleMouseLeave(e)}}
+        onMouseMove={(e) => { handleMouseMove(e) }}
+        onClick={(e) => { handleMouseLeave(e) }}
       >
         <img id="image" />
-        <p className="college">Vidyavardhini's College of Engineering and Technology</p>
-        <p className="present">Presents</p>
-        <h1 className="hackathon"> VCET HACKATHON</h1>
-        <h1 className="moh">MULTIVERSE OF HACKING</h1>
+        <div className="landingContainer" >
+            <h1 className="collegeName">Vidyavardhini's College Of Engineering and Technology </h1>
+            <p className={`text-center mt-4 presents`}>presents</p>
+            <h1 className="mainHeading">VCET HACKATHON</h1>
+            <h3 className="themeName">Multiverse of Hacking</h3>
+          </div>
+        {/* <div className="d-flex flex-column justify-content-center LandingContainer " id="Landing" >
+          
+        </div> */}
       </div>
       <div className="mobile">
-        
+
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Landing
