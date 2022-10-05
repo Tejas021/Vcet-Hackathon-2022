33f79269523bd06b03723d33c7bad839d555b554
Tanmay-Patel-21 <tanmay.202174101@vcet.edu.in>
2022-10-05 14:39:51 +0530

    sponsors updated

--- src/components/sponsor/Sponsor.jsx
+++ src/components/sponsor/Sponsor.jsx
@@ -12,19 +12,19 @@ function Sponsor() {
                     <img src="/sponsors/dear-digital.png" alt="" />
                 </a>
                 <div className="pb1 h2">Powered by</div>
-                <a href="https://internetivo.com/" className="pb-spons-1 sponsor-card" target="_blank">
-                    <img src="/sponsors/internetivo.png" alt="" />
+                <a href="/" className="pb-spons-1 sponsor-card" target="_blank">
+                    <img src="/sponsors/Napsums.png" alt="" />
                 </a>
                 <div className="pb2 h2">Powered by</div>
-                <a href="https://internetivo.com/" className="pb-spons-2 sponsor-card" target="_blank">
-                    <img src="/sponsors/internetivo.png" alt="" />
+                <a href="/" className="pb-spons-2 sponsor-card" target="_blank">
+                    <img src="/sponsors/G_Systems.png" alt="" />
                 </a>
                 <div className="cpb h2">Co-powered by</div>
                 <a href="/" className="cpb-spons sponsor-card" target="_blank">
-                    <img src="/sponsors/un_bank_2021.JPG" alt="" />
+                    <img src="/sponsors/software_ag.png" alt="" />
                 </a>
                 <div className="aws h2">Associated with</div>
-                <div className="ep h3">Education Partner</div>
+                <div className="ep h3">Coding Partner</div>
                 <a href="/" className="eps sponsor-card" target="_blank">
                     <img src="/sponsors/gfg_2021.png" alt="" />
                 </a>
@@ -42,19 +42,19 @@ function Sponsor() {
                 </a>
                 <div className="tp h3">Technology Partner</div>
                 <a href="/" className="tps sponsor-card" target="_blank">
-                    <img src="/sponsors/Kraftpixel.png" alt="" />
+                    <img src="/sponsors/taskade-logo.png" alt="" />
                 </a>
-                <div className="codep h3">Coding Partner</div>
+                <div className="codep h3">Broadcasting Partner</div>
                 <a href="/" className="codeps sponsor-card" target="_blank">
-                    <img src="/sponsors/coding-blocks.png" alt="" />
+                    <img src="/sponsors/cafe_cine_fx.png" alt="" />
                 </a>
-                <div className="kp h3">Knowledge Partner</div>
+                {/* <div className="kp h3">Knowledge Partner</div>
                 <a href="/" className="kps sponsor-card" target="_blank">
                     <img src="/sponsors/upcoming_engineer_2021.png" alt="" />
-                </a>
-                <div className="mp h3">Media Partner</div>
-                <a href="/" className="mps sponsor-card" target="_blank">
-                    <img src="/sponsors/loksatta.png" alt="" />
+                </a> */}
+                <div className="current_mp h3">Media Partner</div>
+                <a href="/" className="current_mps sponsor-card" target="_blank">
+                    <img src="/sponsors/lokmat.png" alt="" />
                 </a>
 
             </div>
